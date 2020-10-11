6040010f2c96843a8d954af8ee375a97c2931589
Yoel Yukalo <yoelyukalo@gmail.com>
2020-10-11 15:08:58 -0500

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,31 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      return true
+    else
+      return false
+    end
   end
 
   def question?
-
+    if self.end_with?("?")
+      return true
+    else
+      return false
+    end
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      return true
+    else
+      return false
+    end
   end
 
   def count_sentences
-
+    array = self.split(/[\.!?] /)
+    array.length
   end
 end
